01bde9734af821b2ac076e302c3a523c13a9bd0b
Elranitacopter <elranacopter@gmail.com>
2024-07-05 19:09:15 -0400

    mysql arreglado

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql");
 module.exports = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "tienda"
+  host: "sql3.freemysqlhosting.net",
+  user: "sql3718198",
+  password: M3Yn1LdVaJ,
+  database: "sql3718198",
+  port : 3306
 });
--- src/db.sql
+++ src/db.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE tienda
 CREATE TABLE productos (
-	id int PRIMARY KEY AUTO_INCREMENT,
-	codigo int,
-	productos varchar(510),
+	id int PRIMARY KEY AUTO_INCREMENT not null,
+	codigo varchar(510),
+	producto varchar(510),
 	categoria_id int,
 	existencia_actual int,
 	precio decimal(11,2)
 );
 
-CREATE TABLE productos (
+CREATE TABLE categorias (
 	id int PRIMARY KEY AUTO_INCREMENT,
     categoria varchar(510) not null
 );
