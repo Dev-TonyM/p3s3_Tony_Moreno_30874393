25172cacb9d5f73d74ab113d6ffb5df56ec22aef
Elranitacopter <elranacopter@gmail.com>
2024-07-05 19:11:34 -0400

    pass arreglada

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 module.exports = mysql.createPool({
   host: "sql3.freemysqlhosting.net",
   user: "sql3718198",
-  password: M3Yn1LdVaJ,
+  password: "M3Yn1LdVaJ",
   database: "sql3718198",
   port : 3306
 });
